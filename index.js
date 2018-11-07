7fb45b0f37a11486ddea8dea6a4d5be434305f22
Satulint <32435994+Satulint@users.noreply.github.com>
2018-11-07 14:31:47 +0200

    index and package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('Hello there');
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+  res.send('Moi');
+});
+ 
+app.listen(3000);
+
+
+
